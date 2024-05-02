db04e9ae71bc422f44c0273c83c45e39dee65b04
paulbknhs <dev@paulbknhs.de>
2024-05-02 08:01:34 +0200

    idk tbh

--- themes/pastel-theme.lua
+++ themes/pastel-theme.lua
@@ -39,7 +39,7 @@ theme.fg_urgent = "#ffffff"
 theme.fg_minimize = "#ffffff"
 
 -- Window Gap Distance
-theme.useless_gap = dpi(12)
+theme.useless_gap = dpi(4)
 
 -- Show Gaps if Only One Client is Visible
 theme.gap_single_client = true
