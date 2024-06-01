7954e289cdac7e0cbc11a1ff25e3e12719312eb4
paulbknhs <dev@paulbknhs.de>
2024-06-01 21:21:10 +0200

    init

---
+++ bindings/mod.lua
@@ -0,0 +1,1 @@
+local modkey = "Mod4"
---
+++ main/user.lua
@@ -0,0 +1,16 @@
+local HOME = os.getenv("HOME")
+local USER = os.getenv("USER")
+
+local modkey = "Mod4"
+
+local _M = {
+	wallpaper = HOME .. "/Pictures/wallpapers/1.jpg",
+
+	terminal = "kitty",
+	browser = "firefox",
+	music = "spotify",
+	email = "thunderbird",
+	editor = "code",
+	filemanager = "nemo",
+	launcher = HOME .. "/.config/rofi/launchers/type-3/launcher.sh",
+}
